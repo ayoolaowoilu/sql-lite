cc0fe40fcde058cd56de6b86d168b49213310428
ayoolaowoilu <ayoolaowoilu@gmail.com>
2026-07-31 02:55:08 +0100

    Feat :  implemented the add functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use crate::utils::read_write_fn::read_db;
+use std::collections::HashMap;
+
+use serde_json::Value;
+
+use crate::utils::read_write_fn::{add_line_to_db_table, read_db};
 
 
 mod utils;
@@ -7,4 +11,12 @@ mod utils;
 fn main() {
     println!("Hello, world!");
     read_db("default");
+
+        let mut row = HashMap::new();
+    row.insert("name".to_string(), Value::String("Dave".to_string()));
+    row.insert("email".to_string(), Value::String("dave@x.com".to_string()));
+    row.insert("id".to_string(), Value::Number(1.into()));
+
+    add_line_to_db_table("default.users", row);
+
 }
