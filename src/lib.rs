edbaafacae501e1540925018a3ce2637a05de741
ayoolaowoilu <ayoolaowoilu@gmail.com>
2026-08-01 02:14:06 +0100

    Feat : Passed all the tests

--- src/lib.rs
+++ src/lib.rs
@@ -66,6 +66,7 @@ use crate::utils::read_write_fn::{BASE_PATH, UpdateProp, UpdateType, add_line_to
             target_value: None
         });
 
+        
         let content = std::fs::read_to_string(format!("{}/testdb/testtable.txt", BASE_PATH)).expect("Failed to read file");
         assert!(content.contains("name->string")); 
     }
